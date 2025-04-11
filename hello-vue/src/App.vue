bb5c84106dcabb9c95914a02a8e2a1772ec35481
HaiTao-G <18838951959@163.com>
2025-04-11 18:29:07 +0800

    feat: hello word脚手架搭建

---
+++ hello-vue/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      msg: "Hello word!",
+    };
+  },
+};
+</script>
+
+<style scoped>
+#app {
+  color: red;
+}
+</style>
