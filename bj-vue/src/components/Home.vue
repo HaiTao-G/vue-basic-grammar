4df34cb177aff5347cf6a5ff702ff4bdbe22acd2
HaiTao-G <18838951959@163.com>
2025-05-07 17:22:55 +0800

    feat：主页添加搜所组件

--- bj-vue/src/components/Home.vue
+++ bj-vue/src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
       <nav-menu></nav-menu>
-        <router-view/>
+      <router-view/>
     </div>
 </template>
 
--- bj-vue/src/components/library/LibraryIndex.vue
+++ bj-vue/src/components/library/LibraryIndex.vue
@@ -2,21 +2,34 @@
   <el-container>
     <el-aside style="width: 200px;margin-top: 20px">
       <switch></switch>
-      <side-menu></side-menu>
+      <SideMenu @indexSelect="listByCategory" ref="sideMenu"></SideMenu>
     </el-aside>
     <el-main>
-      <books class="books-area"></books>
+      <books class="books-area" ref="booksArea"></books>
     </el-main>
   </el-container>
 </template>
 
 <script>
-import SideMenu from './SideMenu.vue'
-import Books from './Books.vue'
-export default {
-  components: { SideMenu ,Books},
-    name: 'LibraryIndex',
-}
+  import SideMenu from './SideMenu'
+  import Books from './Books'
+
+  export default {
+    name: 'AppLibrary',
+    components: {Books, SideMenu},
+    methods: {
+      listByCategory () {
+        var _this = this
+        var cid = this.$refs.sideMenu.cid
+        var url = 'categories/' + cid + '/books'
+        this.$axios.get(url).then(resp => {
+          if (resp && resp.status === 200) {
+            _this.$refs.booksArea.books = resp.data
+          }
+        })
+      }
+    }
+  }
 </script>
 
 <style scoped>
